be524f166c61f218ed8fd80faa457e5597efea40
hyeyeonismm <hyeyeonismm@gmail.com>
2023-11-05 20:46:37 +0900

    feat: 로그인페이지

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {createGlobalStyle} from "styled-components";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+const GlobalStyle = createGlobalStyle`
+ body {
+  letter-spacing: -0.1px;
+  background-color: #000000;
+}
 
 
+.layout{
+    max-width: 390px;
+    background-color: white;
+    margin: 0 auto;
+    min-height:100vh;
+}
+`;
 function App() {
   return (
-    <div className="App">
-      <header>
-        <p>
-          Edit and save to reload.
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+      <div className="layout">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
