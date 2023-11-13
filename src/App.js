2c2c5bbe9c1d09b5153adcdcc03266084e193eff
hyeyeonismm <hyeyeonismm@gmail.com>
2023-11-13 11:08:24 +0900

    test: 반응형 테스트

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {createGlobalStyle} from "styled-components";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Main from "./pages/Main";
-import Card from "./pages/Card";
-import Stock from "./pages/Stock";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Main from './pages/Main';
+import Card from './pages/Card';
+import Stock from './pages/Stock';
 
 const GlobalStyle = createGlobalStyle`
 
- body {
+ body,html {
   letter-spacing: -0.1px;
   background-color: #000000;
+  margin: 0;
+  padding: 0;
 }
-
+ 
 
 .layout{
-    max-width: 390px;
-    max-height: 700px;
-    background-color: white;
-    margin: 0 auto;
-    
+  max-width: 390px;
+  max-height: 700px;
+  background-color: white;
+  padding: 15px;
+  margin: 0 auto; 
 }
 `;
 function App() {
