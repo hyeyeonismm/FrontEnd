2ba10d9d87a831e3d015ff7f92034cb65f101d7b
hahaharin <harin2950@naver.com>
2023-11-18 21:35:13 +0900

    fix : 모달창 뒷배경 효과 수정

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,49 +1,65 @@
 import React, { useState } from 'react';
 import arrow from "../images/arrow.svg";
-import toggle from "../assets/images/toggle.svg"
+import toggle from "../assets/images/toggle.svg";
 // import Toggle from "../Toggle"
 import { useNavigate } from "react-router-dom";
-import { styled, Modal } from "@mui/material";
-import { Grid, Button, Img, Input } from '../components';
-
+import { styled, Modal, Box, Typography } from "@mui/material";
+import { Grid, Button } from '../components';  // Correct the import for Img
 
 function TopNav() {
     const navigate = useNavigate();
-    const [selectedButton, setSelectedButton] = useState("/card"); 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const openModal = () => setIsModalOpen(true);
-    const closeModal = () => setIsModalOpen(false);
-    
-
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedButton, setSelectedButton] = useState("/card");
 
     const handleButtonClick = (path) => {
         setSelectedButton(path);
         navigate(path);
     };
 
+    const openModal = () => {
+        setIsOpen(true);
+    };
 
+    const closeModal = () => {
+        setIsOpen(false);
+    };
 
     return (
         <>
             <Grid theme="topNavContainer">
+                {/* 뒤로가기 버튼 */}
                 <button style={buttonStyle}>
                     <img style={{ width: "13px", height: "26px", marginTop: 7 }} src={arrow} alt="arrow" />
                 </button>
-                {/* <Toggle role="button" onClick={toggleSide} /> */}
-                <button style={buttonStyle}  onClick={openModal}>
-                    <img style={{marginTop: 7 }} src={toggle} alt="toggle" />
+
+                {/* 메뉴 버튼 */}
+                <button style={buttonStyle} onClick={openModal}>
+                    <img style={{ marginTop: 7 }} src={toggle} alt="toggle" />
                 </button>
-                <Modal style={modalStyle} open={openModal} onClose={closeModal}/>
+               
+               {/* 모달 창 */}
+               <Modal
+                    open={isOpen}
+                    onClose={closeModal}
+                    sx={ModalWrap}
+                >
+                    <Box sx={ModalStyle}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                        </Typography>
+                    </Box>
+                </Modal>
+
             </Grid>
 
-
-                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center", marginLeft: "30px", marginRight: "30px" }}>
-                    <Button theme="topNavBtn" onClick={() => handleButtonClick("/card")} selected={selectedButton === "/consumption"}>내 소비</Button>
-                    <Button theme="topNavBtn" onClick={() => handleButtonClick("/stock")} selected={selectedButton === "/stock"}>주주되기</Button>
-                </div>
-                <Grid theme="topNavLine" />
-
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center", marginLeft: "30px", marginRight: "30px" }}>
+                <Button theme="topNavBtn" onClick={() => handleButtonClick("/card")} selected={selectedButton === "/consumption"}>내 소비</Button>
+                <Button theme="topNavBtn" onClick={() => handleButtonClick("/stock")} selected={selectedButton === "/stock"}>주주되기</Button>
+            </div>
+            <Grid theme="topNavLine" />
         </>
     );
 }
@@ -53,18 +69,16 @@ const buttonStyle = {
     border: "none",
     cursor: "pointer",
 };
+const ModalWrap = {
+    margin: "0 auto",
+    width: "390px",
+};
 
-const modalStyle = {
-    position: "fixed",
-    top: 0,
-    right: 0,
-    height: "100%",
-    width: "300px",
-    background: "#fff",
-    zIndex: 9999
-}
-
-
-
+const ModalStyle = {
+    height: "695px",
+    marginTop: "8px",
+    marginLeft: "30%",
+    background: "white",
+};
 
 export default TopNav;
