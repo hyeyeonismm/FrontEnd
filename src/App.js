d6d37f3f5e877814e5d86537d605e4f4027f282c
hyeyeonismm <hyeyeonismm@gmail.com>
2023-11-19 18:28:22 +0900

    fix: 폰트 수정

--- src/App.js
+++ src/App.js
@@ -6,30 +6,33 @@ import Main from './pages/Main.js';
 import Card from './pages/Card';
 import Onboarding from './pages/Onboarding.js';
 import StockApp from './components/Stock/StockApp.js';
-import ONESHINHANLIGHT from './assets/font/ONESHINHANLIGHT.TTF';
+import ROBOTO from './assets/font/Roboto-Medium.ttf';
 
 const GlobalStyle = createGlobalStyle`
 @font-face {
-  font-family: 'ONESHINHANLIGHT';
-  src: local('ONESHINHANLIGHT'), url(${ONESHINHANLIGHT}) format('truetype');
+  font-family: 'ROBOTO';
+  src: local('ROBOTO'), url(${ROBOTO}) format('truetype');
   font-weight: normal;
   font-style: normal;
 }
 
- body {
+ body, html {
   background-color: #000000;
-  font-family: 'ONESHINHANLIGHT';
-  letter-spacing: -1px;
+  letter-spacing: -0.1px;
+  margin: 0;
+  background-color: black;
+  padding: 0;
+  height: 100vh;
+  overflow-x: hidden;
 }
 
 .layout{
-    width: 390px;
-    height: 844px;
+    max-width: 390px;
+    height: 720px;
     background-color: white;
     margin: 0 auto;
     overflow-y: scroll;
     overflow-x: hidden;
-    font-family: 'ONESHINHANLIGHT';
 }
 
 .layout::-webkit-scrollbar {
@@ -41,21 +44,21 @@ const GlobalStyle = createGlobalStyle`
 
 `;
 function App() {
-  return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <div className="layout">
-        <Routes>
-          <Route path="/" element={<Onboarding />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/card" element={<Card />} />
-          <Route path="/stock/*" element={<StockApp />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<GlobalStyle />
+			<div className='layout'>
+				<Routes>
+					<Route path='/' element={<Onboarding />} />
+					<Route path='/main' element={<Main />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/signup' element={<Signup />} />
+					<Route path='/card' element={<Card />} />
+					<Route path='/stock/*' element={<StockApp />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -16,12 +16,12 @@ function Main() {
 		<>
 			<Grid theme='main'>
 				<Grid theme='introTagLine'>소비 데이터 기반 주식 추천 서비스</Grid>
-				<Grid theme='title'>SeedStock</Grid>
+				<Grid theme='title'>LinkStock</Grid>
 				<Grid theme='description'>평범한 일상 속에서 개인화된 주식 투자 기회를 제안합니다.</Grid>
 			</Grid>
 			<Grid theme='main_background'>
 				<Img theme='main' src={main} alt='main' />
-				<Button theme='mainStartBtn' children='시작하기' onClick={() => handleButtonClick('/card')}/>
+				<Button theme='mainStartBtn' children='시작하기' onClick={() => handleButtonClick('/card')} />
 			</Grid>
 		</>
 	);
