201970cba33d850837182db85066555904c34341
hyeyeonismm <hyeyeonismm@gmail.com>
2023-11-20 14:57:42 +0900

    fix: proxy 파일 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,21 @@ module.exports = function (app) {
 	app.use(
 		'/news',
 		createProxyMiddleware({
-			target: 'https://2a1d-118-91-110-133.ngrok-free.app/',
+			target: 'https://398a-118-91-110-133.ngrok-free.app/',
+			changeOrigin: true,
+		}),
+	);
+	app.use(
+		'/auth',
+		createProxyMiddleware({
+			target: 'http://backend-service:8080/',
+			changeOrigin: true,
+		}),
+	);
+	app.use(
+		'/card',
+		createProxyMiddleware({
+			target: 'http://backend-service:8080/',
 			changeOrigin: true,
 		}),
 	);
